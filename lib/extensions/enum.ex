34343cfb571e549616285aff80c0c010799b646e
sorax <robert@hausgedacht.de>
2022-12-06 17:38:48 +0100

    add missing doc

--- lib/extensions/enum.ex
+++ lib/extensions/enum.ex
@@ -3,6 +3,9 @@ defmodule Extensions.Enum do
   Extensions for Enum (like a parallel map function)
   """
 
+  @doc """
+  Analog to Enum.map, but executed in parallel.
+  """
   def pmap(collection, func) do
     collection
     |> Enum.map(&Task.async(fn -> func.(&1) end))
